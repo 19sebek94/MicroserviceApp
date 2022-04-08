6689519d3e3edfeb83ccb27500879a89fc18d7e9
Sebastian Turek <sebastiant1994@users.noreply.github.com>
2022-04-08 23:05:23 +0200

    Add services

---
+++ src/Services/Basket/Basket.API/Mapper/BaskettProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBus.Messages.Events;
+
+namespace Basket.API.Mapper
+{
+    public class BaskettProfile : Profile
+    {
+        public BaskettProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,16 +1,14 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -32,6 +30,16 @@ namespace Basket.API
             });
             services.AddControllers();
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o =>
+            o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            //services.AddMassTransitHostedService();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
